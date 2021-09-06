55b3c4bccc063b8c91baffed5b8c46ac68604896
he-wowers <will.owers@healthengine.com.au>
2021-09-06 14:28:53 +0800

    Added new signup fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import {
+  AmplifySignOut, AmplifyAuthenticator, AmplifyAuthContainer, AmplifySignUp,
+} from '@aws-amplify/ui-react';
 import { Amplify, Auth } from 'aws-amplify';
+import { v4 } from 'uuid';
 import { Home } from './views/Home';
 import { Team as TeamView } from './views/Team/Team';
 import { Player as PlayerView } from './views/Player/Player';
@@ -15,24 +18,84 @@ Amplify.configure(awsConfig);
 
 Auth.configure(awsConfig);
 
-export const App = withAuthenticator(() => (
-  <Router>
-    <div className="App">
-      <AmplifySignOut style={{ backgroundColor: 'blue' }} />
-      <Link to="/team/1">First team</Link>
-      <Link to="/player/8d0ccd7b-64ac-4279-9f5d-d3ee4511bcab">
-        First player
-      </Link>
-    </div>
-    <Route path="/" exact component={Home} />
-    <Route path="/admin" exact component={Admin} />
-    <Route path="/team/:id" component={TeamView} />
-    <Route path="/player/:id" component={PlayerView} />
-    <Route path="/game/:id" component={GameView} />
-    <Route path="/week/:id" component={WeekView} />
-    <Route path="/season/:id" component={SeasonView} />
-  </Router>
-));
+const randomUUID = v4();
+
+export const App = () => (
+  <AmplifyAuthContainer>
+    <AmplifyAuthenticator usernameAlias="email">
+      <Router>
+        <div className="App">
+          <AmplifySignOut style={{ backgroundColor: 'blue' }} />
+          <Link to="/team/1">First team</Link>
+          <Link to="/player/8d0ccd7b-64ac-4279-9f5d-d3ee4511bcab">
+            First player
+          </Link>
+        </div>
+        <Route path="/admin" exact component={Admin} />
+        <Route path="/" exact component={Home} />
+        <Route path="/team/:id" component={TeamView} />
+        <Route path="/player/:id" component={PlayerView} />
+        <Route path="/game/:id" component={GameView} />
+        <Route path="/week/:id" component={WeekView} />
+        <Route path="/season/:id" component={SeasonView} />
+      </Router>
+      <AmplifySignUp
+        slot="sign-up"
+        headerText="Sign up to Marlow Street Ultimate"
+        usernameAlias="email"
+        formFields={[
+          {
+            type: 'email',
+            label: 'Your Email',
+            placeholder: 'E.g elon@musk.com',
+            required: true,
+          },
+          {
+            type: 'password',
+            label: 'Your Password',
+            placeholder: 'YourPassword',
+            inputProps: { required: true, autocomplete: 'new-password' },
+            required: true,
+          },
+          {
+            type: 'given_name',
+            label: 'Your First Name',
+            placeholder: 'Elon',
+            required: true,
+          },
+          {
+            type: 'family_name',
+            label: 'Your Last Name',
+            placeholder: 'Musk',
+            required: true,
+          },
+          {
+            type: 'phone_number',
+            label: 'Your Mobile Number',
+            placeholder: '1300655506',
+            required: true,
+          },
+          {
+            type: 'birthdate',
+            label: 'You date of birth',
+            placeholder: '10/10/1990',
+            required: true,
+          },
+          {
+            type: 'preferred_username',
+            label: '',
+            placeholder: '',
+            value: randomUUID,
+            disabled: true,
+            inputProps: {
+              id: 'randomID',
+            },
+          },
+        ]}
+      />
+    </AmplifyAuthenticator>
+  </AmplifyAuthContainer>
+);
 
 // eslint-disable-next-line import/no-default-export
 export default App;
